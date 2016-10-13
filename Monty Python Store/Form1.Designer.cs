0195b29f16a7cd3dc46e4c264fce9f2e33af6b8d
ManuelMuncaster <russmunc061@ed.amdsb.ca>
2016-10-13 12:56:28 -0400

    Add project files.

---
+++ Monty Python Store/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Monty_Python_Store
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.parrotLabel = new System.Windows.Forms.Label();
+            this.parrotTextbox = new System.Windows.Forms.TextBox();
+            this.helmetLabel = new System.Windows.Forms.Label();
+            this.helmetTextbox = new System.Windows.Forms.TextBox();
+            this.sillywalkLabel = new System.Windows.Forms.Label();
+            this.sillywalkTextbox = new System.Windows.Forms.TextBox();
+            this.albumLabel = new System.Windows.Forms.Label();
+            this.albumTextbox = new System.Windows.Forms.TextBox();
+            this.grenadeLabel = new System.Windows.Forms.Label();
+            this.grenadeTextbox = new System.Windows.Forms.TextBox();
+            this.spanishLabel = new System.Windows.Forms.Label();
+            this.spanishTextbox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.subtotalOutputlabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.taxOutputlabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(12, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(181, 13);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Welcome to the Monty Python Store!";
+            // 
+            // parrotLabel
+            // 
+            this.parrotLabel.AutoSize = true;
+            this.parrotLabel.ForeColor = System.Drawing.Color.White;
+            this.parrotLabel.Location = new System.Drawing.Point(12, 48);
+            this.parrotLabel.Name = "parrotLabel";
+            this.parrotLabel.Size = new System.Drawing.Size(114, 13);
+            this.parrotLabel.TabIndex = 1;
+            this.parrotLabel.Text = "\"\"Dead\"\" Parrot $5.00";
+            // 
+            // parrotTextbox
+            // 
+            this.parrotTextbox.Location = new System.Drawing.Point(135, 45);
+            this.parrotTextbox.Name = "parrotTextbox";
+            this.parrotTextbox.Size = new System.Drawing.Size(25, 20);
+            this.parrotTextbox.TabIndex = 2;
+            // 
+            // helmetLabel
+            // 
+            this.helmetLabel.AutoSize = true;
+            this.helmetLabel.ForeColor = System.Drawing.Color.White;
+            this.helmetLabel.Location = new System.Drawing.Point(12, 78);
+            this.helmetLabel.Name = "helmetLabel";
+            this.helmetLabel.Size = new System.Drawing.Size(147, 13);
+            this.helmetLabel.TabIndex = 3;
+            this.helmetLabel.Text = "Black Knights Helmet  $15.00";
+            // 
+            // helmetTextbox
+            // 
+            this.helmetTextbox.Location = new System.Drawing.Point(165, 71);
+            this.helmetTextbox.Name = "helmetTextbox";
+            this.helmetTextbox.Size = new System.Drawing.Size(25, 20);
+            this.helmetTextbox.TabIndex = 4;
+            // 
+            // sillywalkLabel
+            // 
+            this.sillywalkLabel.AutoSize = true;
+            this.sillywalkLabel.BackColor = System.Drawing.Color.Black;
+            this.sillywalkLabel.ForeColor = System.Drawing.Color.White;
+            this.sillywalkLabel.Location = new System.Drawing.Point(12, 106);
+            this.sillywalkLabel.Name = "sillywalkLabel";
+            this.sillywalkLabel.Size = new System.Drawing.Size(74, 13);
+            this.sillywalkLabel.TabIndex = 5;
+            this.sillywalkLabel.Text = "Silly Walk $20";
+            // 
+            // sillywalkTextbox
+            // 
+            this.sillywalkTextbox.Location = new System.Drawing.Point(92, 103);
+            this.sillywalkTextbox.Name = "sillywalkTextbox";
+            this.sillywalkTextbox.Size = new System.Drawing.Size(25, 20);
+            this.sillywalkTextbox.TabIndex = 6;
+            // 
+            // albumLabel
+            // 
+            this.albumLabel.AutoSize = true;
+            this.albumLabel.ForeColor = System.Drawing.Color.White;
+            this.albumLabel.Location = new System.Drawing.Point(12, 135);
+            this.albumLabel.Name = "albumLabel";
+            this.albumLabel.Size = new System.Drawing.Size(210, 13);
+            this.albumLabel.TabIndex = 7;
+            this.albumLabel.Text = "\"Always look on the bright side\" Album $25";
+            // 
+            // albumTextbox
+            // 
+            this.albumTextbox.Location = new System.Drawing.Point(228, 135);
+            this.albumTextbox.Name = "albumTextbox";
+            this.albumTextbox.Size = new System.Drawing.Size(22, 20);
+            this.albumTextbox.TabIndex = 8;
+            // 
+            // grenadeLabel
+            // 
+            this.grenadeLabel.AutoSize = true;
+            this.grenadeLabel.ForeColor = System.Drawing.Color.White;
+            this.grenadeLabel.Location = new System.Drawing.Point(12, 164);
+            this.grenadeLabel.Name = "grenadeLabel";
+            this.grenadeLabel.Size = new System.Drawing.Size(158, 13);
+            this.grenadeLabel.TabIndex = 9;
+            this.grenadeLabel.Text = "Holy Hand Grenade $1,000,000";
+            // 
+            // grenadeTextbox
+            // 
+            this.grenadeTextbox.Location = new System.Drawing.Point(176, 161);
+            this.grenadeTextbox.Name = "grenadeTextbox";
+            this.grenadeTextbox.Size = new System.Drawing.Size(18, 20);
+            this.grenadeTextbox.TabIndex = 10;
+            // 
+            // spanishLabel
+            // 
+            this.spanishLabel.AutoSize = true;
+            this.spanishLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.spanishLabel.ForeColor = System.Drawing.Color.Red;
+            this.spanishLabel.Location = new System.Drawing.Point(12, 197);
+            this.spanishLabel.Name = "spanishLabel";
+            this.spanishLabel.Size = new System.Drawing.Size(138, 13);
+            this.spanishLabel.TabIndex = 11;
+            this.spanishLabel.Text = "The Spanish Inquisition $50";
+            // 
+            // spanishTextbox
+            // 
+            this.spanishTextbox.Location = new System.Drawing.Point(156, 190);
+            this.spanishTextbox.Name = "spanishTextbox";
+            this.spanishTextbox.Size = new System.Drawing.Size(24, 20);
+            this.spanishTextbox.TabIndex = 12;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.ForeColor = System.Drawing.Color.White;
+            this.calculateButton.Location = new System.Drawing.Point(23, 226);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(153, 23);
+            this.calculateButton.TabIndex = 13;
+            this.calculateButton.Text = "Calculate!";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.BackColor = System.Drawing.Color.Black;
+            this.subtotalLabel.ForeColor = System.Drawing.Color.White;
+            this.subtotalLabel.Location = new System.Drawing.Point(20, 279);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(49, 13);
+            this.subtotalLabel.TabIndex = 14;
+            this.subtotalLabel.Text = "Subtotal:";
+            // 
+            // subtotalOutputlabel
+            // 
+            this.subtotalOutputlabel.AutoSize = true;
+            this.subtotalOutputlabel.ForeColor = System.Drawing.Color.White;
+            this.subtotalOutputlabel.Location = new System.Drawing.Point(75, 279);
+            this.subtotalOutputlabel.Name = "subtotalOutputlabel";
+            this.subtotalOutputlabel.Size = new System.Drawing.Size(35, 13);
+            this.subtotalOutputlabel.TabIndex = 15;
+            this.subtotalOutputlabel.Text = "label1";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.ForeColor = System.Drawing.Color.White;
+            this.taxLabel.Location = new System.Drawing.Point(20, 313);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(28, 13);
+            this.taxLabel.TabIndex = 16;
+            this.taxLabel.Text = "Tax:";
+            // 
+            // taxOutputlabel
+            // 
+            this.taxOutputlabel.AutoSize = true;
+            this.taxOutputlabel.ForeColor = System.Drawing.Color.White;
+            this.taxOutputlabel.Location = new System.Drawing.Point(57, 313);
+            this.taxOutputlabel.Name = "taxOutputlabel";
+            this.taxOutputlabel.Size = new System.Drawing.Size(35, 13);
+            this.taxOutputlabel.TabIndex = 17;
+            this.taxOutputlabel.Text = "label1";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.ForeColor = System.Drawing.Color.White;
+            this.totalLabel.Location = new System.Drawing.Point(23, 349);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 18;
+            this.totalLabel.Text = "Total";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(494, 441);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxOutputlabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalOutputlabel);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.spanishTextbox);
+            this.Controls.Add(this.spanishLabel);
+            this.Controls.Add(this.grenadeTextbox);
+            this.Controls.Add(this.grenadeLabel);
+            this.Controls.Add(this.albumTextbox);
+            this.Controls.Add(this.albumLabel);
+            this.Controls.Add(this.sillywalkTextbox);
+            this.Controls.Add(this.sillywalkLabel);
+            this.Controls.Add(this.helmetTextbox);
+            this.Controls.Add(this.helmetLabel);
+            this.Controls.Add(this.parrotTextbox);
+            this.Controls.Add(this.parrotLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Monty Python Store";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label parrotLabel;
+        private System.Windows.Forms.TextBox parrotTextbox;
+        private System.Windows.Forms.Label helmetLabel;
+        private System.Windows.Forms.TextBox helmetTextbox;
+        private System.Windows.Forms.Label sillywalkLabel;
+        private System.Windows.Forms.TextBox sillywalkTextbox;
+        private System.Windows.Forms.Label albumLabel;
+        private System.Windows.Forms.TextBox albumTextbox;
+        private System.Windows.Forms.Label grenadeLabel;
+        private System.Windows.Forms.TextBox grenadeTextbox;
+        private System.Windows.Forms.Label spanishLabel;
+        private System.Windows.Forms.TextBox spanishTextbox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label subtotalOutputlabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label taxOutputlabel;
+        private System.Windows.Forms.Label totalLabel;
+    }
+}
+
---
+++ Monty Python Store/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monty_Python_Store
+{
+    public partial class Form1 : Form
+    {
+        const double PARROT_COST = 5.00;
+        const double HELMET_COST = 15.00;
+        const double SILLY_WALK_COST = 20.00;
+        const double ALBUM_COST = 25.00;
+        const double GRENADE_COST = 1000000.00;
+        const double SPANISH_COST = 50.00;
+        const double TAX = 0.13;
+        int parrotNumber;
+        int helmetNumber;
+        int sillywalkNumber;
+        int albumNumber;
+        int grenadeNumber;
+        int spanishNumber;
+        double subTotal;
+        double Total;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            parrotNumber = Convert.ToInt32(parrotTextbox.Text);
+            helmetNumber = Convert.ToInt32(helmetTextbox.Text);
+            sillywalkNumber = Convert.ToInt32(sillywalkTextbox.Text);
+            albumNumber = Convert.ToInt32(albumTextbox.Text);
+            grenadeNumber = Convert.ToInt32(grenadeTextbox.Text);
+            spanishNumber = Convert.ToInt32(spanishTextbox.Text);
+
+            subTotal = PARROT_COST * parrotNumber + HELMET_COST * helmetNumber + SILLY_WALK_COST * sillywalkNumber + ALBUM_COST * albumNumber + GRENADE_COST * grenadeNumber + SPANISH_COST * spanishNumber;
+            Total = subTotal * TAX;
+
+            subtotalOutputlabel.Text = "" + subTotal.ToString ("C");
+
+             
+        }
+    }
+}
